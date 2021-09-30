0d747ff088023d403f9a7dfdce1bbde226ba65cc
Jay P Lim <jay@Jays-MacBook-Pro-2.local>
2021-09-30 19:42:17 -0400

    first commit

--- runOverheadTest.sh
+++ runOverheadTest.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-cd overhead_test/crlibm
+cd overhead_test/glibc_rlibm_generic
 ./runAll.sh
 cd ../glibc_double
 ./runAll.sh
 cd ../glibc_float
 ./runAll.sh
-cd ../glibc_rlibm_generic
+cd ../crlibm
 ./runAll.sh
 cd ../glibc_rlibm32
+./runAll.sh
+cd ../intel_rlibm_generic
 ./runAll.sh
 cd ../intel_double
 ./runAll.sh
 cd ../intel_float
-./runAll.sh
-cd ../intel_rlibm_generic
 ./runAll.sh
 cd ../../..
