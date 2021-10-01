10e7b8f8d9be44ac2caaef4045523a4cd84f13ce
Jay P Lim <jay@Jays-MacBook-Pro-2.local>
2021-09-30 20:22:16 -0400

    first commit

--- overhead_test/glibc_double/runAll.sh
+++ overhead_test/glibc_double/runAll.sh
@@ -2,25 +2,25 @@
 echo -e "--- FLOAT WITH GLIBC DOUBLE ---"
 
 echo -e "Log"
-taskset -c 12 ./Log
+taskset -c 0 ./Log
 
 echo -e "Log2"
-taskset -c 12 ./Log2
+taskset -c 0 ./Log2
 
 echo -e "Log10"
-taskset -c 12 ./Log10
+taskset -c 0 ./Log10
 
 echo -e "Exp"
-taskset -c 12 ./Exp
+taskset -c 0 ./Exp
 
 echo -e "Exp2"
-taskset -c 12 ./Exp2
+taskset -c 0 ./Exp2
 
 echo -e "Exp10"
-taskset -c 12 ./Exp10
+taskset -c 0 ./Exp10
 
 echo -e "Sinh"
-taskset -c 12 ./Sinh
+taskset -c 0 ./Sinh
 
 echo -e "Cosh"
-taskset -c 12 ./Cosh
+taskset -c 0 ./Cosh
--- overhead_test/glibc_rlibm_generic/runAll.sh
+++ overhead_test/glibc_rlibm_generic/runAll.sh
@@ -2,31 +2,31 @@
 echo -e "--- FLOAT WITH GCC RLIBM GENERIC ---"
 
 echo -e "Log"
-taskset -c 12 ./Log
+taskset -c 0 ./Log
 
 echo -e "Log2"
-taskset -c 12 ./Log2
+taskset -c 0 ./Log2
 
 echo -e "Log10"
-taskset -c 12 ./Log10
+taskset -c 0 ./Log10
 
 echo -e "Exp"
-taskset -c 12 ./Exp
+taskset -c 0 ./Exp
 
 echo -e "Exp2"
-taskset -c 12 ./Exp2
+taskset -c 0 ./Exp2
 
 echo -e "Exp10"
-taskset -c 12 ./Exp10
+taskset -c 0 ./Exp10
 
 echo -e "Sinh"
-taskset -c 12 ./Sinh
+taskset -c 0 ./Sinh
 
 echo -e "Cosh"
-taskset -c 12 ./Cosh
+taskset -c 0 ./Cosh
 
 echo -e "Sinpi"
-taskset -c 12 ./Sinpi
+taskset -c 0 ./Sinpi
 
 echo -e "Cospi"
-taskset -c 12 ./Cospi
+taskset -c 0 ./Cospi
