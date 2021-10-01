762f35f9993b70fcfee4a03a5df29eb350b1960f
Jay P Lim <jay@Jays-MacBook-Pro-2.local>
2021-10-01 15:07:50 -0400

    corr test

--- correct_test/testFloatResults/RunTest.sh
+++ correct_test/testFloatResults/RunTest.sh
@@ -10,3 +10,75 @@ echo -n "  |  "
 echo -n "  |  "
 ./intel_double/Log
 echo    "  |"
+
+echo -n "| log2(x)  |  "
+./rlibm_Ofa/Log2
+echo -n "  |  "
+./glibc_double/Log2
+echo -n "  |  "
+./intel_double/Log2
+echo    "  |"
+
+echo -n "| log10(x) |  "
+./rlibm_Ofa/Log10
+echo -n "  |  "
+./glibc_double/Log10
+echo -n "  |  "
+./intel_double/Log10
+echo    "  |"
+
+echo -n "| exp(x)   |  "
+./rlibm_Ofa/Exp
+echo -n "  |  "
+./glibc_double/Exp
+echo -n "  |  "
+./intel_double/Exp
+echo    "  |"
+
+echo -n "| exp2(x)  |  "
+./rlibm_Ofa/Exp2
+echo -n "  |  "
+./glibc_double/Exp2
+echo -n "  |  "
+./intel_double/Exp2
+echo    "  |"
+
+echo -n "| exp10(x) |  "
+./rlibm_Ofa/Exp10
+echo -n "  |  "
+./glibc_double/Exp10
+echo -n "  |  "
+./intel_double/Exp10
+echo    "  |"
+
+echo -n "| sinh(x)  |  "
+./rlibm_Ofa/Sinh
+echo -n "  |  "
+./glibc_double/Sinh
+echo -n "  |  "
+./intel_double/Sinh
+echo    "  |"
+
+echo -n "| cosh(x)  |  "
+./rlibm_Ofa/Cosh
+echo -n "  |  "
+./glibc_double/Cosh
+echo -n "  |  "
+./intel_double/Cosh
+echo    "  |"
+
+echo -n "| sinpi(x) |  "
+./rlibm_Ofa/Sinpi
+echo -n "  |  "
+echo -n "                 "
+echo -n "  |  "
+./intel_double/Sinpi
+echo    "  |"
+
+echo -n "| cospi(x) |  "
+./rlibm_Ofa/Cospi
+echo -n "  |  "
+echo -n "                 "
+echo -n "  |  "
+./intel_double/Cospi
+echo    "  |"
