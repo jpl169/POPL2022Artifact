b53ec070d5a01cb1f6ccab9154840f266eecc30c
Jay P Lim <jay@Jays-MacBook-Pro-2.local>
2021-10-01 15:01:21 -0400

    corr test

--- correct_test/testFloatResults/RunTest.sh
+++ correct_test/testFloatResults/RunTest.sh
@@ -3,10 +3,10 @@
 echo    "| function |    Using OurLibm    |    glibc dbl libm   |   Intel dbl libm    |"
 echo    "----------------------------------------------------------------------------"
 echo    "|          | rne rnn rnp rnz rna | rne rnn rnp rnz rna | rne rnn rnp rnz rna |"
-echo -n "| ln(x)    | "
+echo -n "| ln(x)    |  "
 ./rlibm_Ofa/Log
-echo -n "  | "
+echo -n "  |  "
 ./glibc_double/Log
-echo -n "  | "
+echo -n "  |  "
 ./intel_double/Log
 echo    "  |"
