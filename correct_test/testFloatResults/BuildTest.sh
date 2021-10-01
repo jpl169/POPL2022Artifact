38ca47e6f09955b1e8e816fdfdfe1270da604aee
Jay P Lim <jay@Jays-MacBook-Pro-2.local>
2021-10-01 14:54:48 -0400

    corr test

--- correct_test/testFloatResults/BuildTest.sh
+++ correct_test/testFloatResults/BuildTest.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 cd ../rlibm_Ofa
-make -clean
+make clean
 make -s
 cd ../glibc_double
-make -clean
+make clean
 make -s
 cd ../intel_double
-make -clean
+make clean
 make -s
 cd crlibm
-make -clean
+make clean
 make -s
 cd ../rlibm32
-make -clean
+make clean
 make -s
 cd ..
