c3c983e172f388ae00e7b4cc19ca90de0872a415
Jay P Lim <jay@jays-mbp-2.lan>
2021-10-01 20:43:18 -0400

    corr test

--- correct_test/testFloatResults/RunTest.sh
+++ correct_test/testFloatResults/RunTest.sh
@@ -82,3 +82,68 @@ echo -n "                 "
 echo -n "  |  "
 ./intel_double/Cospi
 echo    "  |"
+
+
+
+echo    "| function |    Using Crlibm     |    Using Rlibm32    |"
+echo    "--------------------------------------------------------"
+echo    "|          | rne rnn rnp rnz rna | rne rnn rnp rnz rna |"
+echo -n "| ln(x)    |  "
+./crlibm/Log
+echo -n "  |  "
+./rlibm32/Log
+echo    "  |"
+
+echo -n "| log2(x)  |  "
+./crlibm/Log2
+echo -n "  |  "
+./rlibm32/Log2
+echo    "  |"
+
+echo -n "| log10(x) |  "
+./crlibm/Log10
+echo -n "  |  "
+./rlibm32/Log10
+echo    "  |"
+
+echo -n "| exp(x)   |  "
+./crlibm/Exp
+echo -n "  |  "
+./rlibm32/Exp
+echo    "  |"
+
+echo -n "| exp2(x)  |  "
+echo -n "                 "
+echo -n "  |  "
+./rlibm32/Exp2
+echo    "  |"
+
+echo -n "| exp10(x) |  "
+echo -n "                 "
+echo -n "  |  "
+./rlibm32/Exp10
+echo    "  |"
+
+echo -n "| sinh(x)  |  "
+./crlibm/Sinh
+echo -n "  |  "
+./rlibm32/Sinh
+echo    "  |"
+
+echo -n "| cosh(x)  |  "
+./crlibm/Cosh
+echo -n "  |  "
+./rlibm32/Cosh
+echo    "  |"
+
+echo -n "| sinpi(x) |  "
+./crlibm/Sinpi
+echo -n "  |  "
+./rlibm32/Sinpi
+echo    "  |"
+
+echo -n "| cospi(x) |  "
+./crlibm/Cospi
+echo -n "  |  "
+./rlibm32/Cospi
+echo    "  |"
