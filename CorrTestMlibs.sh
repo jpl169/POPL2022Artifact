303736276c800b3b5ac17a932b803672e817e935
Jay P Lim <jay@jays-mbp-2.lan>
2021-10-02 18:56:31 -0400

    corr test

--- CorrTestMlibs.sh
+++ CorrTestMlibs.sh
@@ -9,7 +9,7 @@ echo -e "\033[1mFloat Result Correctness Test \033[0m"
 ./CleanTest.sh
 
 echo -e "\033[1mBuilding Test Suite for Tensorfloat32 Result \033[0m"
-cd ../testTensorfloat32Results
+cd ../testTensorFloat32Results
 ./BuildTest.sh
 
 echo -e "\033[1mTensorfloat32 Result Correctness Test \033[0m"
