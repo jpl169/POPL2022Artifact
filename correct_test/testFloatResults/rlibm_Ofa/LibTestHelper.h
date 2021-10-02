ee11fb0233e1ef1b8310feaa0ee60a7ff36b4925
Jay P Lim <jay@jays-mbp-2.lan>
2021-10-02 18:45:01 -0400

    corr test

--- correct_test/testFloatResults/rlibm_Ofa/LibTestHelper.h
+++ correct_test/testFloatResults/rlibm_Ofa/LibTestHelper.h
@@ -60,7 +60,7 @@ void RunTestForExponent() {
     fx.x = count;
     double res = __ELEM__(fx.f);
     
-    for (int rnd_index = 0; rnd_index < 5; rnd_index++) {
+    for (int rnd_index = 0; rnd_index < 5; rnd_index += 0x100) {
       mpfr_rnd_t rnd = rnd_modes[rnd_index];
       float oracleResult = MpfrResult(fx.f, rnd);
       float roundedRes = RoundDoubleToF8N(res, 32, my_rnd_modes[rnd_index]);
