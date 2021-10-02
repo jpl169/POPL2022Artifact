20d57f5d24cc62ecbd0f8a04e3248ef034f6c0be
Jay P Lim <jay@jays-mbp-2.lan>
2021-10-02 14:11:28 -0400

    corr test

--- include/rounding.h
+++ include/rounding.h
@@ -241,6 +241,11 @@ float RoundDoubleToFEN(double v, int explength, int bitlength, enum RoundMode rn
       exp += 127;
     }
     
+    if (v == 6.9314718055994528622676398299518041312694549560546875e-01) {
+      printf("mantissa = %lx\n", mantissa);
+      printf("exp = %lx\n", exp);
+    }
+    
     unsigned long infExt = ((unsigned long)exp << (64 - 1 - explength)) | mantissa;
     
     // 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000
