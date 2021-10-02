5d6734358177df61bb989bb5d59cdfc3d22861d3
Jay P Lim <jay@jays-mbp-2.lan>
2021-10-02 18:32:07 -0400

    corr test

--- CorrTestMlibs.sh
+++ CorrTestMlibs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e "\033[1mBuilding Test Suite for Float Result \033[0m"
-cd corr_test/testFloatResults
+cd correct_test/testFloatResults
 ./BuildTest.sh
 
 echo -e "\033[1mFloat Result Correctness Test \033[0m"
