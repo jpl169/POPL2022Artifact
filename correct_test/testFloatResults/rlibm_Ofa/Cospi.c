f52c8db6b76011c0c05fbe47466ef89452468fab
Jay P Lim <jay@jays-mbp-2.lan>
2021-10-01 20:36:37 -0400

    corr test

--- correct_test/testFloatResults/rlibm_Ofa/Cospi.c
+++ correct_test/testFloatResults/rlibm_Ofa/Cospi.c
@@ -4,30 +4,23 @@
 
 int IsSpecialCase(float x, double* specCaseRes) {
   if (x < 0) x = -x;
-  int sticky = 0;
-  double xp = (double)x * 2.0;
-  int N = (int)xp;
-  double R = x - N * 0.5;
-  
   double reduced = fmod(x, 2.0);
   
-  if (R == 0) {
-    if (reduced == 0.0) {
-      *specCaseRes = 1.0;
-      return 1;
-    }
-    else if (reduced == 0.5) {
-      *specCaseRes = 0.0;
-      return 1;
-    }
-    else if (reduced == 1.0) {
-      *specCaseRes = -1.0;
-      return 1;
-    }
-    else if (reduced == 1.5) {
-      *specCaseRes = 0.0;
-      return 1;
-    }
+  if (reduced == 0.0) {
+    *specCaseRes = 1.0;
+    return 1;
+  }
+  else if (reduced == 0.5) {
+    *specCaseRes = 0.0;
+    return 1;
+  }
+  else if (reduced == 1.0) {
+    *specCaseRes = -1.0;
+    return 1;
+  }
+  else if (reduced == 1.5) {
+    *specCaseRes = 0.0;
+    return 1;
   }
   
   return 0;
