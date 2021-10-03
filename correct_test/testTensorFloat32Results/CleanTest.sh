fef73a1bed1210d987e4bec1dcc6cee5040a1b03
Jay P Lim <jay@jays-mbp-2.lan>
2021-10-02 20:03:04 -0400

    corr test

--- correct_test/testTensorFloat32Results/CleanTest.sh
+++ correct_test/testTensorFloat32Results/CleanTest.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-cd ../rlibm_Ofa
+cd rlibm_Ofa
 make -s clean
 cd ../glibc_double
 make -s clean
 cd ../intel_double
 make -s clean
-cd crlibm
+cd ../crlibm
 make -s clean
 cd ../rlibm32
 make -s clean
