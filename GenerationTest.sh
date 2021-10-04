aff9ccc23268bb7a51bb556f4901b68e5346ea9e
Jay P Lim <jay@jays-mbp-2.lan>
2021-10-04 14:19:56 -0400

    generation

---
+++ GenerationTest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo -e "\033[1mGenerating Log2(x) for inputs x in [1, 2) \033[0m"
+echo -e "\033[1mGenerating oracle file \033[0m"
+cd correct_test/testFloatResults
+./BuildTest.sh
+
