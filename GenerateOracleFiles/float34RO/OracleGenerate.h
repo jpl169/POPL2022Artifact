a960054ebf8cf2bbc48afab0c526a1dc251029d5
Jay P Lim <jay@jays-mbp-2.lan>
2021-10-04 13:34:29 -0400

    generation

--- GenerateOracleFiles/float34RO/OracleGenerate.h
+++ GenerateOracleFiles/float34RO/OracleGenerate.h
@@ -6,22 +6,27 @@ mpfr_t mval;
 
 double ComputeOracleResult(float x);
 
-void RunTest(char* FileName) {
-    unsigned long count = 0;
+void RunTestHelper(char* FileName, unsigned long start, unsigned long end) {
+  unsigned long count = 0;
   
-    float x;
-    floatX xbase;
-
-    FILE* f = fopen(FileName, "w");
+  float x;
+  floatX xbase;
   
-    for (count = 0x0; count < 0x100000000; count++) {
-        xbase.x = count;
-        x = xbase.f;
+  FILE* f = fopen(FileName, "w");
+  
+  for (count = start; count < end; count++) {
+    xbase.x = count;
+    x = xbase.f;
     
-        double res = ComputeOracleResult(x);
+    double res = ComputeOracleResult(x);
+    
+    fwrite(&res, sizeof(double), 1, f);
+  }
+  
+  fclose(f);
 
-        fwrite(&res, sizeof(double), 1, f);
-    }
+}
 
-    fclose(f);
+void RunTest(char* FileName) {
+  RunTestHelper(FileName, 0x0, 0x100000000);
 }
