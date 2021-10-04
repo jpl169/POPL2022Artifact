b28c23a82745e0fe546a90117f72c325967fce93
Jay P Lim <jay@jays-mbp-2.lan>
2021-10-04 14:37:22 -0400

    generation

--- GenerationTest.sh
+++ GenerationTest.sh
@@ -2,10 +2,13 @@
 
 echo -e "\033[1mGenerating Log2(x) for inputs x in [1, 2) \033[0m"
 echo -e "\033[1mGenerating oracle file \033[0m"
-mkdir -p intervals
+mkdir -p oracles
 cd GenerateOracleFiles
 make -s Log2Small
 ./Log2Small ../oracles/Log2SmallOracle
 
-
+mkdir -p intervals
+cd IntervalGen
+make -s Log2Small
+./Log2Small ../oracles/Log2SmallOracle ../intervals/Log2SmallIntervals
 
