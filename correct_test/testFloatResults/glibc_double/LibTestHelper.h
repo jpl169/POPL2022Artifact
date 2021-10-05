73627e417f174e41285b25b8cc71200c2de6fc0f
Jay P Lim <jay@jays-mbp-2.lan>
2021-10-05 11:16:14 -0400

    test

--- correct_test/testFloatResults/glibc_double/LibTestHelper.h
+++ correct_test/testFloatResults/glibc_double/LibTestHelper.h
@@ -24,22 +24,10 @@ void RunTestForExponent() {
     fx.x = count;
     double orc = __GEN_ELEM__(fx.f);
     double res = __ELEM__(fx.f);
-    
-    if (fx.f == 5.49830607545632921019073167360000000000000000000000e+28) {
-      printf("rlibm = %.50e (%lx)\n", orc, *(unsigned long*)&orc);
-      printf("glibc = %.50e (%lx)\n", res, *(unsigned long*)&res);
-    }
 
     for (int rnd_index = 0; rnd_index < 5; rnd_index++) {
       double oracleResult = RoundDoubleToF8N(orc, 32, my_rnd_modes[rnd_index]);
       double roundedRes = RoundDoubleToF8N(res, 32, my_rnd_modes[rnd_index]);
-
-      if (fx.f == 5.49830607545632921019073167360000000000000000000000e+28) {
-        printf("rnd   = %d\n", rnd_index);
-        printf("x     = %.50e\n", fx.f);
-        printf("orc   = %.50e\n", oracleResult);
-        printf("res   = %.50e\n", roundedRes);
-      }
       
       if (oracleResult != oracleResult && roundedRes != roundedRes) continue;
       if (oracleResult != roundedRes) {
